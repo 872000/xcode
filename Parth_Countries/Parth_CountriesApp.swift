88975a7c6fb5495337ecf965caebea97e6c57bef
parth <parthmanchanda87@gmail.com>
2023-12-06 13:50:07 -0500

    Initial Commit

---
+++ Parth_Countries/Parth_CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parth_CountriesApp.swift
+//  Parth_Countries
+//
+//  Created by Parth Manchanda on 2023-12-06.
+//
+
+import SwiftUI
+
+@main
+struct Parth_CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
